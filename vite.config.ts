19d27a1f936e3b1ac191bb0e10438d73c765883e
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-10-07 10:41:23 +0200

    Cleans up vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ declare const __dirname: string;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { lib: { entry: resolve(__dirname, "src/index.ts"), formats: ["umd"], fileName: "index" } },
   resolve: { alias: { src: resolve("src/") } },
   plugins: [dts({ include: ["src"] })],
 });
