a3d656243db171f7003262c437b0e417608880c1
Alexis Jacomy <alexis.jacomy@gmail.com>
2024-10-07 11:43:47 +0200

    Fixes Vite base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ declare const __dirname: string;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "graphology-layout-forceatlas2-gpu",
   resolve: { alias: { src: resolve("src/") } },
   plugins: [dts({ include: ["src"] })],
 });
